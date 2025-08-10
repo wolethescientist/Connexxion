0aa6ca8a267ee23a3ea2ad4def375d92b58bc375
David Aiyewumi <davidaiyewumi1@gmail.com>
2025-08-10 17:55:43 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -316,6 +316,6 @@ async def public_chat_with_bot(
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, workers=4, proxy_headers=True, forwarded_allow_ips="*")
+    uvicorn.run("main:app", host="0.0.0.0", port=port, workers=1, proxy_headers=True, forwarded_allow_ips="*")
 
 
